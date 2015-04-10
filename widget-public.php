5788a096d9fe339cebe1b8d6783aa7ebd0166845
bobcares_plugins <bobcares_plugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-10 06:44:23 +0000

    Adding stable version tag
    git-svn-id: https://plugins.svn.wordpress.org/daily-quotes/tags/1.0@1131785 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ widget-public.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * PHP File - Created on Sept, 26
+ * @copyright Copyright 2014, poornam.com
+ * @author    Bobcares <geethu.n@poornam.com>
+ *
+ */
+?>
+
+<!-- Added division to display border color -->
+<div style="border:10px solid <?php echo $instance['border'] ?>;">
+    <div class="quote-widget-color<?php echo $instance['color'] ?>">
+        <div class="quote-widget-title"><?php echo $title ?></div>
+
+
+        <div class="quote-widget-author-details">
+            <div class="quote-author-image">
+                <img src="<?php echo $author->image ?>" alt="<?php echo $author->name ?> <?php __('Image', $this->plugin_slug) ?>">
+            </div>
+
+            <div class="quotes-author">
+                <?php echo $author->name ?>
+
+                <div class="quote-widget-author-lived"><?php echo $author->lived ?></div>
+
+            </div>
+
+            <div class="quote-widget-body">
+                <?php echo $quote ?>
+            </div>
+
+
+        </div>
+    </div>
